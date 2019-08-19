0ca9e0fc70b7030cafc50e009bbee7d2b25225da
Manu <reloading@gmx.ch>
2019-08-19 20:47:23 +0200

    Created DAL Folder and added HotelContext with several DbSets...

---
+++ HotelJerbourg/HotelJerbourg/DAL/HotelContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using HotelJerbourg.Models;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace HotelJerbourg.DAL
+{
+    public class HotelContext : DbContext
+    {
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<RoomCategory> RoomCategories { get; set; }
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Bill> Bills { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<Room>().
+
+            //modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            //modelBuilder.Entity<Course>()
+            //    .HasMany(c => c.Instructors).WithMany(i => i.Courses)
+            //    .Map(t => t.MapLeftKey("CourseID")
+            //        .MapRightKey("InstructorID")
+            //        .ToTable("CourseInstructor"));
+
+            //modelBuilder.Entity<Department>().MapToStoredProcedures();
+        }
+    }
+}
