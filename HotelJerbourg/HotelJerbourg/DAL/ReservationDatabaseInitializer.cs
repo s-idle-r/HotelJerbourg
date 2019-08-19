796947982b85413ed18be05cea6104c7d77c6ace
Manu <reloading@gmx.ch>
2019-08-19 20:56:29 +0200

    Created DatabaseInitialiser AKA SEED Method....

---
+++ HotelJerbourg/HotelJerbourg/DAL/ReservationDatabaseInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Collections.Generic;
+using System.Data.Entity;
+using HotelJerbourg.Models;
+
+namespace HotelJerbourg.DAL
+{
+    public class ReservationDatabaseInitializer : DropCreateDatabaseIfModelChanges<HotelContext>
+    {
+        protected override void Seed(HotelContext context)
+        {
+            
+        }
+
+        private static Hotel GetHotel()
+        {
+            var
+        }
+    }
+}
